275d387e6b3957c835b25602c9cb7918c807381f
Michał <mmichalf@gmail.com>
2013-04-22 20:07:13 +0200

    poprawki v2

---
+++ MPD_CES_Algorithm/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPD_CES_Algorithm
+{
+    public class Task
+    {
+        public Int32 T;
+        public Int32 P;
+        public Int32 D;
+        public Color Color;
+    }
+}
